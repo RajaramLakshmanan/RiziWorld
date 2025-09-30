07aa032ae4246ff3a83b8d41f405f2a4b4fb5cf8
RajaramLakshmanan <lrajarammca@gmail.com>
2025-09-30 19:22:49 +0530

    RR : Init

---
+++ RiziApp/rizisdk/src/main/java/com/example/rizisdk/RiziSDK.java
@@ -0,0 +1,9 @@
+package com.example.rizisdk;
+
+import android.util.Log;
+
+public class RiziSDK {
+    public static void riziWorld(){
+        Log.d("Rizi world","Welcome to Rizi world");
+    }
+}
